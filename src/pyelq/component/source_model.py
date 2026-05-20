efa49af61d87cbf3ebabf24382a108b316295a85
david.randell <david.randell@shell.com>
2026-05-20 13:45:46 +0200

    updated logic around calling generate_sources

--- src/pyelq/component/source_model.py
+++ src/pyelq/component/source_model.py
@@ -583,6 +583,9 @@ class SourceModel(Component, SourceGrouping, SourceDistribution):
                 they will be required when we need to update the couplings when new source locations are proposed when
                 we move/birth/death.
 
+        If no sources are in the source map, generate_sources will be run with default arguments to generate an initial
+        source map with sources in the coverage area.
+
         Args:
             sensor_object (SensorGroup): object containing sensor data.
             meteorology (MeteorologyGroup): object containing meteorology data.
@@ -592,6 +595,9 @@ class SourceModel(Component, SourceGrouping, SourceDistribution):
         self.sensor_object = sensor_object
         if self.dispersion_model is None:
             self.initialise_dispersion_model(sensor_object, meteorology, gas_species)
+
+        if self.dispersion_model.source_map.nof_sources == 0:
+            self.generate_sources(sensor_object=sensor_object, meteorology=meteorology, gas_species=gas_species)
         else:
             self.coupling = self.dispersion_model.compute_coupling(
                 sensor_object, meteorology, gas_species, output_stacked=True
@@ -617,35 +623,31 @@ class SourceModel(Component, SourceGrouping, SourceDistribution):
         If a dispersion_model has not already been attached to the instance, then this function adds a GaussianPlume
         dispersion model, with a default source map that has limits set based on the site limits
 
-        If no sources are in the source map, generate_sources will be run with default arguments to generate an initial
-        source map with sources in the coverage area.
 
         Args:
             sensor_object (SensorGroup): object containing sensor data.
             meteorology (MeteorologyGroup): object containing meteorology data.
             gas_species (GasSpecies): object containing gas species information.
-        """
-        if self.dispersion_model is None:
-            blank_source_map = SourceMap()
-            sensor_locations = sensor_object.location.to_enu()
-            blank_source_map.location = ENU(
-                ref_latitude=sensor_locations.ref_latitude,
-                ref_longitude=sensor_locations.ref_longitude,
-                ref_altitude=sensor_locations.ref_altitude,
-            )
-            if self.site_limits is None:
-                buffer = 1
-                self.site_limits = np.array(
-                    [
-                        [np.min(sensor_locations.east) - buffer, np.max(sensor_locations.east) + buffer],
-                        [np.min(sensor_locations.north) - buffer, np.max(sensor_locations.north) + buffer],
-                        [np.min(sensor_locations.up) - buffer, np.max(sensor_locations.up) + buffer],
-                    ]
-                )
-            self.dispersion_model = GaussianPlume(blank_source_map)
 
-        if self.nof_sources == 0:
-            self.generate_sources(sensor_object=sensor_object, meteorology=meteorology, gas_species=gas_species)
+        """
+
+        blank_source_map = SourceMap()
+        sensor_locations = sensor_object.location.to_enu()
+        blank_source_map.location = ENU(
+            ref_latitude=sensor_locations.ref_latitude,
+            ref_longitude=sensor_locations.ref_longitude,
+            ref_altitude=sensor_locations.ref_altitude,
+        )
+        if self.site_limits is None:
+            buffer = 1
+            self.site_limits = np.array(
+                [
+                    [np.min(sensor_locations.east) - buffer, np.max(sensor_locations.east) + buffer],
+                    [np.min(sensor_locations.north) - buffer, np.max(sensor_locations.north) + buffer],
+                    [np.min(sensor_locations.up) - buffer, np.max(sensor_locations.up) + buffer],
+                ]
+            )
+        self.dispersion_model = GaussianPlume(blank_source_map)
 
     def generate_sources(
         self,
@@ -654,11 +656,11 @@ class SourceModel(Component, SourceGrouping, SourceDistribution):
         gas_species: GasSpecies,
         nof_sources: int = 5,
     ):
-        """Generate random source locations for the source map using a hyper_cube design within coverage area.
+        """Generate random source locations for the source map using a hypercube design within coverage area.
 
-        Attempts to generate a source map within the site_limits and with all sources in the coverage area. If no
-        sources are in the coverage area, the function will keep trying to generate a source map until it finds one with
-        sources in the coverage area, or until it has tried 100 times (in which case it raises a ValueError).
+        Attempts to generate a source map within the site_limits and with at least one source  in the coverage area. If
+        no sources are in the coverage area, the function will keep trying to generate a source map until it finds one
+        with sources in the coverage area, or until it has tried 100 times (in which case it raises a ValueError).
 
         Args:
             sensor_object (SensorGroup): object containing sensor data.
@@ -667,7 +669,8 @@ class SourceModel(Component, SourceGrouping, SourceDistribution):
             nof_sources (int, optional): number of sources to generate. Defaults to 5.
 
         Raises:
-            ValueError: If no source map with sources in the coverage area can be generated after 100 attempts.
+            ValueError: If no source map with sources in the coverage area can be generated after 50 attempts.
+
         """
         self.sensor_object = sensor_object
         sensor_locations = sensor_object.location.to_enu()
