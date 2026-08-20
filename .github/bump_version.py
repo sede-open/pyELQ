667f400c5a6dd1b1d69a2c37388ddafa5c92b0e8
david.randell <david.randell@shell.com>
2026-08-20 09:18:31 +0200

    updated yml files to fix issues on PR/main with write permissions and issue in bumpy version wirting to main branch

--- .github/bump_version.py
+++ .github/bump_version.py
@@ -11,9 +11,10 @@ old_semantic_version = re.findall(r'version = "(\d+\.\d+\.[a-zA-Z0-9]+)"', versi
 major_version, minor_version, patch_version = old_semantic_version[0].split(".")
 patch_version = int(re.findall(r"\d+", patch_version)[0])
 new_semantic_version = f"{major_version}.{minor_version}.{patch_version + 1}"
-regex_bumped_patch_version = f"\g<1>{new_semantic_version}"
-# Match regex for <version = "0.0.0a"> pattern
-bumped_version_content = re.sub(r'(version = ")\d+\.\d+\.[a-zA-Z0-9]+', regex_bumped_patch_version, version_content)
-with open("pyproject.toml", "w") as file:
-    file.write(bumped_version_content)
-print(new_semantic_version)  # Print is required for release in GitHub action
+old_semantic_version = f"{major_version}.{minor_version}.{patch_version}"
+# regex_bumped_patch_version = f"\g<1>{new_semantic_version}"
+# # Match regex for <version = "0.0.0a"> pattern
+# bumped_version_content = re.sub(r'(version = ")\d+\.\d+\.[a-zA-Z0-9]+', regex_bumped_patch_version, version_content)
+# with open("pyproject.toml", "w") as file:
+#     file.write(bumped_version_content)
+print(old_semantic_version)  # Print is required for release in GitHub action
